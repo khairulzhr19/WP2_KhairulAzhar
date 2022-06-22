8f2293a487e9180297d26c7e6449d59c21013650
khairulzhr19 <101612244+khairulzhr19@users.noreply.github.com>
2022-06-22 19:02:29 +0700

    Add files via upload

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -1,11 +1,12 @@
 <?php
-class Contoh1 extends CI_Controller
+class contoh1 extends CI_Controller
 {
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Khairul Azhar
-            Saya tinggal di daerah Bintaro
-            Olahraga yang saya sukai adalah berenang";
+        echo"Nama saya Khairul Azhar
+        Saya tinggal di daerah Bintaro
+        Olahraga yang saya sukai adalah berenang";
     }
+    
 }
